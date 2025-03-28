74781ecc537701212c4e1e406baa038c4436af3d
UTKARSH GUPTA <utkarsh.gupta0311@gmail.com>
2025-03-28 09:31:29 +0000

    coversion of datatypes

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,22 @@
+let score = 31
+let score1 = "31"
+let score2 = "31abc"
+
+console.log(typeof score);
+console.log(typeof score1);
+console.log(typeof score2);
+
+let valueInNumber = Number(score1);
+let valueInNumber1 = Number(score2);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+console.log(typeof valueInNumber1);
+console.log(valueInNumber1);
+console.log(typeof NaN);
+
+
+
+
+// 1 = true; 0 = false
+// "" = false
+// "hitesh" = true
