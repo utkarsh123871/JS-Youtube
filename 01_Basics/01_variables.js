e715b1c77262682cd6ae396cdd215bc8149842e6
UTKARSH GUPTA <utkarsh.gupta0311@gmail.com>
2025-03-28 09:00:03 +0000

    learnt about variables and datatypes

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 69
+let accountEmail = "hitesh@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+// accountId = 2
+accountEmail = "utkarsh@gmail.com"
+accountPassword = "212121"
+accountCity = "Bangalore"
+
+console.table([accountEmail , accountId , accountPassword , accountCity])
---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,11 @@
+"use strict";
+
+// alert(3+3) // we are using node js not browser
+
+// console.log(3+3)
+// console.log("utkarsh")
+console.log(typeof null)
+console.log(typeof undefined)
+
+
+
